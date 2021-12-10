f9ba1fe7b8a909f54fd2426140ad068cfd1950e9
Masheba <nikolaiageev1991@gmail.com>
2021-12-10 13:38:43 +0300

    build.gradle,.appveyor.yml changed. Failed test added

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldCheckDeliveryCardForm() {
+        Configuration.headless = true;
+
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Самара");
+
+        String planningDate = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date']  input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Ageev AS");
+        $("[data-test-id=phone] input").setValue("+79277334312");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+
+    }
+}
